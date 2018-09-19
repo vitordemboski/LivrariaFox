452a3acd23771e2e374a6a9960e5c708fbbeb168
vitor.7716 <vitor.7716@L-1-17-06--27.satc.edu.br>
2018-09-19 15:27:43 -0300

    Cadastro FEITO TESTADO E APROVADO

--- src/main/java/br/com/projetolivraria/LivrariaFox/controller/CadastroClienteController.java
+++ src/main/java/br/com/projetolivraria/LivrariaFox/controller/CadastroClienteController.java
@@ -5,7 +5,13 @@
  */
 package br.com.projetolivraria.LivrariaFox.controller;
 
+import br.com.projetolivraria.LivrariaFox.models.Cliente;
+import br.com.projetolivraria.LivrariaFox.service.ClienteService;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,8 +21,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class CadastroClienteController {
+    
+    @Autowired
+    private ClienteService cs;
+    
     @RequestMapping(value = "/cadastrarUsuario", method = RequestMethod.GET)
-    public String evento() {
+    public String evento(ModelMap modelMap) {
+        modelMap.put("cliente", new Cliente());
         return "cadastro";
     }
+    
+    @RequestMapping(value = "/cadastrarUsuario", method = RequestMethod.POST)
+    public String cadastrar(@ModelAttribute("cliente") Cliente cliente, ModelMap modelMap, HttpSession session){       
+        if (cliente!=null) {   
+          cs.save(cliente);
+            return "index"; 
+        }else{
+            modelMap.put("error", "Cadastro invalido");
+         return "cadastro";
+        }
+      
+    }
+   
 }
--- src/main/java/br/com/projetolivraria/LivrariaFox/models/Livro.java
+++ src/main/java/br/com/projetolivraria/LivrariaFox/models/Livro.java
@@ -20,7 +20,7 @@ import javax.validation.constraints.NotEmpty;
  * @author usuario
  */
 
-@Entity(name = "Livro")
+@Entity
 public class Livro implements Serializable {
      private static final long serialVersionUID = 1L;
     @Id 
